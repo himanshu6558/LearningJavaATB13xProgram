f7a601c5ed3f2b1de21e046af871d03e70893828
Himanshu Bhandari <himanshubhandari6558@gmail.com>
2025-06-10 08:00:03 +0530

    10 June

---
+++ src/ex_01_Java_Basic/Lab002_Comment.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comment {
+    public static void main(String[] args) {
+        /*System.out.println("Hello test");
+        System.out.println("Hello test2");*/
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab003_Variable.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Variable {
+    public static void main(String[] args) {
+        boolean a = false;
+        byte b = 10;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
