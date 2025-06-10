b1bb49e3bcc8d135a18e035145ad0bd850ea7962
Himanshu Bhandari <himanshubhandari6558@gmail.com>
2025-06-10 08:11:24 +0530

    10 June 2nd Commit

--- src/ex_01_Java_Basic/Lab004_Table.java
+++ src/ex_01_Java_Basic/Lab004_Table.java
@@ -4,16 +4,15 @@ public class Lab004_Table {
     public static void main(String[] args) {
         int a = 9;
         int b = 1;
-        int c = 9;
-        System.out.printf("%d X %d = %d \n",a,b,c);
-        System.out.printf("%d X %d = %d \n",a,b+1,c*2);
-        System.out.printf("%d X %d = %d \n",a,b+2,c*3);
-        System.out.printf("%d X %d = %d \n",a,b+3,c*4);
-        System.out.printf("%d X %d = %d \n",a,b+4,c*5);
-        System.out.printf("%d X %d = %d \n",a,b+5,c*6);
-        System.out.printf("%d X %d = %d \n",a,b+6,c*7);
-        System.out.printf("%d X %d = %d \n",a,b+7,c*8);
-        System.out.printf("%d X %d = %d \n",a,b+8,c*9);
-        System.out.printf("%d X %d = %d \n",a,b+9,c*10);
+        System.out.printf("%d X %d = %d \n",a,b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
+        System.out.printf("%d X %d = %d \n",a,++b,a*b);
     }
 }
