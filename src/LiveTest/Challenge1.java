941b31eeaf5b7801c4595df35417149d1d0e3ed8
Himanshu Bhandari <himanshubhandari6558@gmail.com>
2025-06-19 07:38:13 +0530

    Challenge1 commit

---
+++ src/LiveTest/Challenge1.java
@@ -0,0 +1,12 @@
+package LiveTest;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        int a = 25;
+        double b = 3.14;
+        boolean c = true;
+        char d = 'A';
+
+        System.out.println("Integer: "+a+" Double: "+b+" Boolean: "+c+" Character: "+d);
+    }
+}
