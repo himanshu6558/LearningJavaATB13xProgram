fc49bd732b6cac12a3501f1384fc6ff825e6f076
Himanshu Bhandari <himanshubhandari6558@gmail.com>
2025-06-19 08:07:40 +0530

    Challenge8 commit

---
+++ src/LiveTest/Challenge15.java
@@ -0,0 +1,16 @@
+package LiveTest;
+
+public class Challenge15 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        int n=2;
+        for(int i=2;i<=100;i++){
+            if(n%i==0)
+            {
+                System.out.println(n);
+                n++;
+            }
+
+        }
+    }
+}
